a14cab760fbd00b78133280c35f61e9d00373cae
Lars van der Niet <larsvanderniet@gmail.com>
2022-01-26 11:35:55 +0000

    Finished responsive layout for tag page

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -7,15 +7,13 @@
 @section('content')
     <div class="container">
         <div class="row vh-100">
-            <div class="col-md my-auto d-flex justify-content-center flex-column text-center">
+            <div class="col-lg my-auto d-flex justify-content-center flex-column text-center">
                 <p class="taginfo">Vul hier de code van jouw kaart in</p>
                 <input class="taginput" type="text" placeholder="Jouw tag-code">
                 <button class="secondary-btn my-4" type="button">Doorgaan</button>
             </div>
-            <div class="col-md my-auto">
-                <div class="col-lg my-auto">
-                    <img class="profielkaart" src="{{ asset('images/kaart.png') }}" alt="Profielkaart voorbeeld">
-                </div>
+            <div class="col-lg my-auto d-none d-lg-block">
+                <img class="profielkaart" src="{{ asset('images/kaart.png') }}" alt="Profielkaart voorbeeld">
             </div>
         </div>
     </div>
